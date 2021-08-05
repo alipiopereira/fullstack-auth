c198792a7810a4c7194d47d9acce0bb86e67a43d
alipiopereira <allipiopereira@gmail.com>
2021-08-05 12:28:21 -0300

    criando o component de Navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+    <vs-navbar square center-collapsed class="navbar">
+      <template #left>
+        <h4>FullStackBeta Auth</h4>
+      </template>
+      <template #right>
+        <vs-button flat to="/login">Login</vs-button>
+        <vs-button>Get Started</vs-button>
+      </template>
+    </vs-navbar>
+</template>
+
+<script setup>
+</script>
+
+<style>
+.navbar {
+  border-bottom: 1.5px #eee solid;
+}
+</style>
