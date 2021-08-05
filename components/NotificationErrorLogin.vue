cc3ad2264552d070dc60b6e8c51f2e333b519652
alipiopereira <allipiopereira@gmail.com>
2021-08-05 12:31:04 -0300

    criando o component de notificacao p erro de login

---
+++ components/NotificationErrorLogin.vue
@@ -0,0 +1,38 @@
+<template>
+  <vs-row>
+    <vs-col w="12" justify="center">
+      <vs-row justify="center" align="center">
+        <vs-col w="2" align="center">
+          <vs-button shadow icon class="pm-0">😢</vs-button>
+        </vs-col>
+
+        <vs-col w="10">
+          <p class="font-size-9">
+            Opa! Percebemos que você fez várias tentativas...
+          </p>
+        </vs-col>
+      </vs-row>
+
+      <vs-row justify="center" align="center" class="mt-1">
+        <vs-col w="2" align="center">
+          <vs-button shadow icon class="pm-0">😀</vs-button>
+        </vs-col>
+        <vs-col w="10">
+          <p class="font-size-9">Você pode entrar usando seu email</p>
+        </vs-col>
+      </vs-row>
+
+      <vs-row justify="center" align="center">
+        <vs-col w="2" align="center">
+          <vs-button shadow icon class="pm-0">😂</vs-button>
+        </vs-col>
+        <vs-col w="10">
+          <p class="font-size-9">Você pode recuperar sua senha</p>
+        </vs-col>
+      </vs-row>
+    </vs-col>
+  </vs-row>
+</template>
+
+<script setup>
+</script>
