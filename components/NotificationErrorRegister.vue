1a5949f7be44f7c714f5d9782e9160914c520718
alipiopereira <allipiopereira@gmail.com>
2021-08-05 12:39:21 -0300

    criando o component p notificar error de register

---
+++ components/NotificationErrorRegister.vue
@@ -0,0 +1,29 @@
+<template>
+  <vs-row>
+    <vs-col w="12" justify="center">
+      <vs-row justify="center" align="center">
+        <vs-col w="2" align="center">
+          <vs-button shadow icon class="pm-0">😢</vs-button>
+        </vs-col>
+
+        <vs-col w="10">
+          <p class="font-size-9">
+            Opa! Percebemos que você fez várias tentativas...
+          </p>
+        </vs-col>
+      </vs-row>
+
+      <vs-row justify="center" align="center" class="mt-1">
+        <vs-col w="2" align="center">
+          <DialogRulesRegister />
+        </vs-col>
+        <vs-col w="10">
+          <p class="font-size-9">Você pode ver as regras aqui</p>
+        </vs-col>
+      </vs-row>
+    </vs-col>
+  </vs-row>
+</template>
+
+<script setup>
+</script>
